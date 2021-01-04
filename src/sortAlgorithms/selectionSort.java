9fb52d32462285c968fb34ae5ffd38cf85528572
Diesel-19 <aselimulgur@gmail.com>
2021-01-04 22:00:43 +0100

    added more sort types

--- src/sortAlgorithms/selectionSort.java
+++ src/sortAlgorithms/selectionSort.java
@@ -2,5 +2,32 @@ package sortAlgorithms;
 
 public class selectionSort {
     //unstable sort algorithm
+    public static void main(String[] args) {
+        int[] array = {20, 35, -15, 7, 55, 1, -22};
 
+        for (int lastUnsortedIndex = array.length - 1; lastUnsortedIndex > 0; lastUnsortedIndex--) {
+            int largest = 0;
+
+            for (int i = 1; i <= lastUnsortedIndex; i++) {
+                if (array[i] > array[largest]) {
+                    largest = i;
+                }
+                swap(array, largest, lastUnsortedIndex);
+            }
+        }
+
+        for (int i : array) {
+            System.out.println(i);
+        }
+    }
+
+    public static void swap(int[] array, int i, int j) {
+        if (i == j) {
+            return;
+        }
+
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
 }
---
+++ src/sortAlgorithms/shellSort.java
@@ -0,0 +1,4 @@
+package sortAlgorithms;
+
+public class shellSort {
+}
